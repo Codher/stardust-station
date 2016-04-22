0a80a32f6e9d0371777cb1d9821852b813811435
Raul Pineda <raul@raulpineda.mx>
2016-04-22 13:08:31 +0200

    Updated project

--- location-timeline/app/scripts/main.js
+++ location-timeline/app/scripts/main.js
@@ -1,17 +1,23 @@
-const DEFAULT_LAT = 55.6761;
+const DEFAULT_LAT = 55.6761; // Copenhagen
 const DEFAULT_LON = 12.5683;
+const DEFAULT_DATE = "2014-02-01";
 const API_KEY = "VIwJlML4oRVW9QylKOgmCH4w6D4h6e9ewhG06sil";
+// const API_KEY = "DEMO_KEY";
 
-function getImages(lat, lon) {
+function getImages(lat, lon, date) {
   $(".figure-container").empty();
   $.get("https://api.nasa.gov/planetary/earth/assets?",
     {
       lat: lat,
       lon: lon,
-      begin: "2014-02-01",
+      begin: date,
       api_key: API_KEY
     }).done(function(assets) {
         if(assets.count > 0) {
+          assets.results.forEach(function(asset) {
+            var figure = $("<figure/>").addClass("location-figure pull-left").attr("id", asset.id);
+            $(".figure-container").append(figure);
+          });
           renderImages(assets.results, lat, lon);
         } else {
           $(".figure-container").html("No images found :(")
@@ -21,35 +27,30 @@ function getImages(lat, lon) {
 }
 
 function renderImages(images, lat, lon) {
-  for (var i = 0; i < 5; i++) {
-    var date = images[i].date.split("T")[0];
-    $.get("https://api.nasa.gov/planetary/earth/imagery",
-      {
-        lat: lat,
-        lon: lon,
-        id: images[i].id,
-        cloud_score: true,
-        date: date,
-        api_key: API_KEY
-      }).done(function(imagery) {
-          if(imagery.cloud_score < 0.2) {
-            var figure = $("<figure/>").addClass("location-figure pull-left");
-            $(".figure-container").append(figure);
-            var img = $("<img/>").attr("src", imagery.url);
-            var caption = $("<figcaption/>").addClass("text-center").text(imagery.date);
-            figure.append(img);
-            figure.append(caption);
+  for (var i = 0; i < 10; i++) {
+    if(images[i]) {
+      var date = images[i].date.split("T")[0];
+      $.get("https://api.nasa.gov/planetary/earth/imagery",
+        {
+          lat: lat,
+          lon: lon,
+          id: images[i].id,
+          cloud_score: "True",
+          date: date,
+          api_key: API_KEY
+        }).done(function(imagery) {
+            if(imagery.cloud_score < 0.1) {
+              var figure = document.getElementById(imagery.id);
+              var $figure = $(figure);
+              var img = $("<img/>").attr("src", imagery.url);
+              var caption = $("<figcaption/>").addClass("text-center").text(imagery.date);
+              $figure.append(img);
+              $figure.append(caption);
+            }
           }
-        }
-    );
+      );
+    }
   }
-}
-
-function randomizeImages() {
-  $(".location-figure").each(function(i, figure) {
-    var index = parseInt(String(Math.random())[3]);
-    $(figure).css('z-index', index);
-  });
 }
 
 function getRandomCoordinates() {
@@ -59,20 +60,17 @@ function getRandomCoordinates() {
   $(".input-lon").val(lon);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $(".submit-coordinates").click(function(e) {
     e.preventDefault();
     var lat = $(".input-lat").val() || DEFAULT_LAT;
     var lon = $(".input-lon").val() || DEFAULT_LON;
-    console.log("Default:", DEFAULT_LAT, DEFAULT_LON);
-    console.log(lat, lon);
-    getImages(lat, lon);
+    var date = $(".input-date").val() || DEFAULT_DATE;
+    getImages(lat, lon, date);
   });
 
   $(".randomize-coordinates").click(function(e) {
     e.preventDefault();
     getRandomCoordinates();
   });
-
-  setInterval(randomizeImages, 1000);
 });
